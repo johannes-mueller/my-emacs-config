c7e43548bb86cb0337c733600029d70e93f3311f
Johannes Mueller <github@johannes-mueller.org>
2020-12-30 22:01:52 +0100

    Switch to Hasklig font

--- lisp/look.el
+++ lisp/look.el
@@ -1,3 +1,10 @@
 
-(set-face-attribute 'fill-column-indicator nil :font "Source Code Pro" :height 200)
-(set-face-attribute 'default nil :background "LightYellow" :foreground "black" :font "Roboto Mono" :height 110)
+;(set-face-attribute 'default nil :background "LightYellow" :foreground "black" :font "Meslo LG M" :height 110)
+;(set-face-attribute 'default nil :background "LightYellow" :foreground "black" :font "Roboto Mono" :height 110)
+;(set-face-attribute 'default nil :background "LightYellow" :foreground "black" :font "Sometype Mono" :height 120)
+;(set-face-attribute 'default nil :background "LightYellow" :foreground "black" :font "Office Code Pro" :height 110)
+(set-face-attribute 'default nil :font "Hasklig" :height 110)
+;(set-face-attribute 'bold nil :font "Hasklig Medium" :height 110)
+(set-face-attribute 'italic nil :font "Hasklig Italic" :height 110)
+
+;(set-face-attribute 'fill-column-indicator nil :font "Source Code Pro" :height 200)
