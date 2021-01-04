6dbe0fc8edc43eb4f3321fa0c8c4cb56b22c0419
Johannes Mueller <github@johannes-mueller.org>
2021-01-04 14:11:38 +0100

    Introduce git meta file modes

--- init.el
+++ init.el
@@ -145,6 +145,11 @@
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
+
+(use-package gitignore-mode)
+(use-package gitattributes-mode)
+(use-package gitconfig-mode)
+
 (use-package forge
   :after magit
   )
