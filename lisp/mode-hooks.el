0c4fbf0c9ba3110de2e44dd5ddbfef478d16b948
Johannes Mueller <github@johannes-mueller.org>
2021-03-23 08:10:46 +0100

    switch back from pyright to pyls

--- lisp/mode-hooks.el
+++ lisp/mode-hooks.el
@@ -73,13 +73,9 @@
 (use-package py-isort
   :after python)
 
-(use-package lsp-pyright
-  :after lsp)
-
 (add-hook 'python-mode-hook
 	  (lambda ()
 	    (johmue/auto-activate-virtualenv)
-	    (require 'lsp-pyright)
 	    (lsp)
 	    (require 'dap-python)
 	    (setq dap-python-debugger 'debugpy)))
