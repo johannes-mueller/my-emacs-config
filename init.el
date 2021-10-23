a9109e82f937047f783a259c98fb1a26202a2401
Johannes Mueller <github@johannes-mueller.org>
2021-10-23 15:41:59 +0200

    Introduce git-modes

--- init.el
+++ init.el
@@ -258,6 +258,8 @@
   :after magit
   )
 
+(use-package git-modes)
+
 (use-package diff-hl
   :hook ((magit-pre-refresh . diff-hl-magit-pre-refresh)
 	 (magit-post-refresh . diff-hl-magit-post-refresh))
