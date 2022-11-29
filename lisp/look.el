4f7b861e04137e59632966b01a1356accb8b0095
Johannes Mueller <github@johannes-mueller.org>
2022-11-29 12:44:02 +0100

    Make faces scale with default font

--- lisp/look.el
+++ lisp/look.el
@@ -5,7 +5,14 @@
 ;(set-face-attribute 'default nil :background "LightYellow" :foreground "black" :font "Office Code Pro" :height 110)
 (set-face-attribute 'default nil :font "Hasklig" :height 110)
 ;(set-face-attribute 'bold nil :font "Hasklig Medium" :height 110)
-(set-face-attribute 'italic nil :font "Hasklig Italic" :height 110)
-(set-face-attribute 'fixed-pitch nil :font "Hasklig Light")
+(set-face-attribute 'italic nil :family "Hasklig Italic" :inherit 'default)
+(set-face-attribute 'fixed-pitch nil :family "Hasklig Light" :inherit 'default)
 
 ;(set-face-attribute 'fill-column-indicator nil :font "Source Code Pro" :height 200)
+
+
+(set-face-attribute 'blamer-face nil
+                    :foreground "#7a88cf"
+                    :slant 'italic
+                    :inherit 'default)
+(set-face-attribute 'corfu-default nil :inherit 'default)
