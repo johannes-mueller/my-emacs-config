be9155cc9ce79249986412ed9a61180ec07a0fc2
Johannes Mueller <github@johannes-mueller.org>
2023-09-23 20:00:41 +0200

    Make *Package-Lint* windows easily closable

--- init.el
+++ init.el
@@ -99,6 +99,7 @@
   (push '("*Help*" :height 0.5 :position top) popwin:special-display-config)
   (push '("*xref*" :height 0.3 :position bottom) popwin:special-display-config)
   (push '("*grep*" :height 0.5 :position bottom) popwin:special-display-config)
+  (push '("*Package-Lint*" :height 0.5 :position bottom) popwin:special-display-config)
   (push '(python-pytest-mode :height 0.5 :position bottom) popwin:special-display-config)
   (push '(magit-mode :position right) popwin:special-display-config)
   (popwin-mode 1)
