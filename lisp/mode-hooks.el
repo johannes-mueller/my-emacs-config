fcff6a911543e314ce1b77ed151e798961532b1a
Johannes Mueller <github@johannes-mueller.org>
2024-06-01 18:03:09 +0200

    Use dape

--- lisp/mode-hooks.el
+++ lisp/mode-hooks.el
@@ -273,4 +273,48 @@
 
 
 ;(use-package flycheck-elixir)
+
+
+(use-package dape
+  :preface
+  ;; By default dape shares the same keybinding prefix as `gud'
+  ;; If you do not want to use any prefix, set it to nil.
+  (setq dape-key-prefix "\C-x\C-d")
+
+  :hook
+  (dape-repl-mode . (lambda () (corfu-mode -1) (message "corfu mode disabled")))
+  ;; Save breakpoints on quit
+  ;; ((kill-emacs . dape-breakpoint-save)
+  ;; Load breakpoints on startup
+  ;;  (after-init . dape-breakpoint-load))
+
+  :init
+  ;; To use window configuration like gud (gdb-mi)
+  (setq dape-buffer-window-arrangement 'right)
+
+  :config
+  ;; Info buffers to the right
+  ;; (setq dape-buffer-window-arrangement 'right)
+
+  ;; Global bindings for setting breakpoints with mouse
+  (dape-breakpoint-global-mode)
+
+  ;; To not display info and/or buffers on startup
+  ;(remove-hook 'dape-on-start-hooks 'dape-info)
+  ;(remove-hook 'dape-on-start-hooks 'dape-repl)
+
+  ;; To display info and/or repl buffers on stopped
+  ;; (add-hook 'dape-on-stopped-hooks 'dape-info)
+  ;; (add-hook 'dape-on-stopped-hooks 'dape-repl)
+
+  ;; Kill compile buffer on build success
+  ;; (add-hook 'dape-compile-compile-hooks 'kill-buffer)
+
+  ;; Save buffers on startup, useful for interpreted languages
+  ;; (add-hook 'dape-on-start-hooks (lambda () (save-some-buffers t t)))
+
+  ;; Projectile users
+  (setq dape-cwd-fn 'projectile-project-root)
+  )
+
 ;;; mode-hooks.el ends here
