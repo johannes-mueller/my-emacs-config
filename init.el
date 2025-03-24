10484d3220f5dc118e21ad7b23ae9c52a0a14549
Johannes Mueller <github@johannes-mueller.org>
2025-03-24 21:06:19 +0100

    Optimize eglot

--- init.el
+++ init.el
@@ -364,8 +364,13 @@
   (add-to-list 'eglot-server-programs `((elixir-ts-mode elixir-mode) . ("elixir-ls")))
   (add-to-list 'eglot-server-programs `((prisma-ts-mode) . ("prisma-language-server" "--stdio")))
   (add-to-list 'eglot-server-programs `((python-ts-mode) . ("basedpyright-langserver" "--stdio")))
+  (add-to-list 'eglot-server-programs `((dockerfile-ts-mode) . ("docker-langserver" "--stdio")))
   (setq eglot-report-progress nil)
-  (setq eglot-events-buffer-config '(:size 0 :format full)))
+  (setq eglot-events-buffer-config '(:size 0 :format full))
+  (setq eglot-prefer-plaintext t)
+  (setq jsonrpc-event-hook nil)
+  :hook
+  ((dockerfile-ts-mode . #'eglot-ensure)))
 
 (use-package flycheck-eglot
   :init
